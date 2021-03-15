663e7f16aa708988edd505ca46ac7962103fb01c
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-15 19:00:52 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,32 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "testez votre age",
+      home: MyCustomForm(),
     );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Fill this out in the next step.
+    return Scaffold(appBar: AppBar(title: Text('lecture de valeur'),
+    ),
+    )
   }
 }
