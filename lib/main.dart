1fb919e6f6dc71520d0bcb346d7ffbf3d386c147
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-15 18:49:57 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(Myapp());
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "testez votre age",
+    );
+  }
+}
